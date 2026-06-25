98149bd242264a9bc9f02783adf41c51d856b965
ahmed-abualsaud <ahmed.m.abualsaud@gmail.com>
2026-06-25 19:56:30 +0300

    Add case sensitivity demonstration in Python variable names

---
+++ 002-case-sensitivity.py
@@ -0,0 +1,12 @@
+# This code demonstrates case sensitivity in Python variable names.
+y=5
+Y=10
+sum=y+Y
+
+# boolean values are case sensitive in Python, there is no 'True' or 'False' in lowercase, they must be capitalized
+yes=True
+no=False
+
+print(sum)
+print(yes)
+print(no)
