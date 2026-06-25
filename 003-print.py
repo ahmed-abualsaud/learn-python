26cc81759929ae450f544809d52e2a2483419a07
ahmed-abualsaud <ahmed.m.abualsaud@gmail.com>
2026-06-25 20:22:53 +0300

    Add print statements to display user information

---
+++ 003-print.py
@@ -0,0 +1,8 @@
+name="ahmed"
+age=20
+email="ahmed@example.com"
+
+# print multiple variables in a single print statement with a comma separator
+print("Name:", name)
+print("Age:", age)
+print("Email:", email)
