00336451e9ee45a02c1cff4af49b3aaa751248f2
ahmed-abualsaud <ahmed.m.abualsaud@gmail.com>
2026-07-14 19:33:02 +0300

    Add examples of while loops and user input handling in 031-while-loop.py

---
+++ 031-while-loop.py
@@ -0,0 +1,48 @@
+n=int(input("Enter a number: ")) # this line will take an integer input from the user and store it in the variable n
+while n > 0:
+    print("Hello, World!:", n) # this line will be printed 10 times with the value of n from 10 to 1
+    n -= 1 # this line will decrement the value of n by 1 in each iteration
+print('Done!') # this line will be printed after the loop is finished
+
+
+print('\n')
+
+
+while True:
+    n = int(input("Enter a number: ")) # this line will take an integer input from the user and store it in the variable n
+    if n <= 0:
+        print("Negative number entered. Exiting loop.") # this line will be printed when the user enters a negative number
+        break # this line will break the loop when n is less than 0
+    else:
+        print("No, number is greater than 0:", n) # this line will be printed with the value of n from the user input
+print('Done!') # this line will be printed after the loop is finished
+
+
+print('\n')
+
+
+n = int(input("Enter a number: ")) # this line will take an integer input from the user and store it in the variable n
+while n > 0:
+    print("Hello, World!:", n) # this line will be printed with the value of n from the user input
+    n = int(input("Enter a number: ")) # this line will take an integer input from the user and store it in the variable n
+print('Done!') # this line will be printed after the loop is finished
+
+
+print('\n')
+
+
+n = 10
+while n :
+    print("Hello, World!:", n) # this line will be printed 10 times with the value of n from 10 to 1
+    n -= 1 # this line will decrement the value of n by 1 in each iteration
+
+
+print('\n')
+
+
+n =0
+while n < 10:
+    print('Number:', n)
+    n += 1 # this line will increment the value of n by 1 in each iteration
+else:
+    print('Done!') # this line will be printed after the loop is finished
